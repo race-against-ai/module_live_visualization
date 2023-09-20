5bcfd634c09f75d50d2292a672e6750cf0d83c13
Calvin Teuber <calvinteuber7@gmail.com>
2023-09-20 08:53:11 +0200

    Improvements to name input skript

--- testing/name_input_sketch.py
+++ testing/name_input_sketch.py
@@ -19,6 +19,7 @@ class DriverDataPublisher:
         self.driver_entry = tk.Entry(self.app)
         self.send_button = tk.Button(self.app, text="Send Data", command=self.send_data)
         self.status_label = tk.Label(self.app, text="")
+        self.app.bind('<Return>', lambda event=None: self.send_data())
 
         self.driver_label.grid(row=0, column=0, padx=10, pady=10)
         self.driver_entry.grid(row=0, column=1, padx=10, pady=10)
@@ -29,8 +30,11 @@ class DriverDataPublisher:
         current_driver = self.driver_entry.get()
         data = "current_driver: " + current_driver
         self.pub_socket.send(data.encode('utf-8'))
-        print("Sending data: " + data)
         self.status_label.config(text="Data sent: " + data)
+        
+        # Clear the Entry widget after sending data
+        self.driver_entry.delete(0, 'end')
+
 
     def run(self):
         self.app.mainloop()
